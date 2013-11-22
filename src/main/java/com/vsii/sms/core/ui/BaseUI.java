9dbdd3169082959f55c84e3dadceb3b9553f04ae
daind <daind@daind-PC.vsii.com>
2013-11-22 13:46:22 +0700

    add layout

--- src/main/java/com/vsii/sms/core/ui/BaseUI.java
+++ src/main/java/com/vsii/sms/core/ui/BaseUI.java
@@ -1,27 +1,83 @@
 package com.vsii.sms.core.ui;
 
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.AbsoluteLayout;
 import com.vaadin.ui.Component;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
-public abstract class BaseUI extends UI 
-{
+public abstract class BaseUI extends UI {
 	private static final long serialVersionUID = 8208880542928342967L;
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.vaadin.ui.UI#init(com.vaadin.server.VaadinRequest)
+	 */
 	@Override
-	protected void init(VaadinRequest request)
-	{
-		VerticalLayout mainLayout = new VerticalLayout();
+	protected void init(VaadinRequest request) {
+		AbsoluteLayout mainLayout = new AbsoluteLayout();
+		// add a border to the layout with CSS to indicate its boundaries
+		mainLayout.addStyleName("border");
+		// allow border to show (100% would clip the right side border)
+		mainLayout.setWidth("100%");
+		mainLayout.setHeight("100%");
 		setContent(mainLayout);
-		
-		mainLayout.addComponent(initHeader());
-		mainLayout.addComponent(initContent());
-		mainLayout.addComponent(initFooter());
-	}
-	
-	protected abstract Component initHeader();
-	protected abstract Component initContent();
-	protected abstract Component initFooter();
 
+		mainLayout.addComponent(initHeader(), "top:5px; left:10px; height: 20%");
+		mainLayout.addComponent(initContent(), "top:30px; right:10px; height: 80%");
+		mainLayout.addComponent(initFooter(), "bottom:0px; right:10px;");
+	}
+
+	private Component initHeader() {
+		Component component = getCustomizeHeader();
+		if (null == component) {
+			component = getDefaultHeader();
+		}
+		return component;
+	}
+
+	protected abstract Component getCustomizeHeader();
+
+	private Component getDefaultHeader() {
+		VerticalLayout headerLayout = new VerticalLayout();
+		Label topright = new Label("Header");
+		headerLayout.addComponent(topright);
+		return headerLayout;
+	}
+
+	private Component initContent() {
+		Component component = getCustomizeContent();
+		if (null == component) {
+			component = getDefaultContent();
+		}
+		return component;
+	}
+
+	protected abstract Component getCustomizeContent();
+
+	private Component getDefaultContent() {
+		VerticalLayout headerLayout = new VerticalLayout();
+		Label topright = new Label("Content");
+		headerLayout.addComponent(topright);
+		return headerLayout;
+	}
+
+	private Component initFooter() {
+		Component component = getCustomizeFooter();
+		if (null == component) {
+			component = getDefaultFooter();
+		}
+		return component;
+	}
+
+	protected abstract Component getCustomizeFooter();
+
+	private Component getDefaultFooter() {
+		VerticalLayout headerLayout = new VerticalLayout();
+		Label topright = new Label("Footer");
+		headerLayout.addComponent(topright);
+		return headerLayout;
+	}
 }
--- src/main/java/com/vsii/sms/core/ui/SmsUI.java
+++ src/main/java/com/vsii/sms/core/ui/SmsUI.java
@@ -2,29 +2,107 @@ package com.vsii.sms.core.ui;
 
 import com.vaadin.annotations.Title;
 import com.vaadin.ui.Component;
-import com.vaadin.ui.Label;
-import com.vsii.sms.core.resource.Messages;
+import com.vaadin.ui.MenuBar;
+import com.vaadin.ui.MenuBar.Command;
+import com.vaadin.ui.MenuBar.MenuItem;
+import com.vaadin.ui.VerticalLayout;
 
 @Title("Servey System")
-public class SmsUI extends BaseUI
-{
+public class SmsUI extends BaseUI {
 	private static final long serialVersionUID = 9148621939177316667L;
+	private MenuBar menubar = new MenuBar();
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.vsii.sms.core.ui.BaseUI#getCustomizeHeader()
+	 */
 	@Override
-	protected Component initHeader()
-	{
-		return new Label(Messages.get("SmsUI.Header"));
+	protected Component getCustomizeHeader() {
+		return null;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.vsii.sms.core.ui.BaseUI#getCustomizeFooter()
+	 */
 	@Override
-	protected Component initContent()
-	{
-		return new Label(Messages.get("SmsUI.Content"));
+	protected Component getCustomizeFooter() {
+		return null;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.vsii.sms.core.ui.BaseUI#getCustomizeContent()
+	 */
 	@Override
-	protected Component initFooter()
-	{
-		return new Label(Messages.get("SmsUI.Footer"));
+	protected Component getCustomizeContent() {
+		VerticalLayout verticalLayout = new VerticalLayout();
+
+		// Save reference to individual items so we can add sub-menu items to
+		// them
+		final MenuBar.MenuItem file = menubar.addItem("File", null);
+		final MenuBar.MenuItem newItem = file.addItem("New", null);
+		file.addItem("Open file...", menuCommand);
+		file.addSeparator();
+
+		newItem.addItem("File", menuCommand);
+		newItem.addItem("Folder", menuCommand);
+		newItem.addItem("Project...", menuCommand);
+
+		file.addItem("Close", menuCommand);
+		file.addItem("Close All", menuCommand);
+		file.addSeparator();
+
+		file.addItem("Save", menuCommand);
+		file.addItem("Save As...", menuCommand);
+		file.addItem("Save All", menuCommand);
+
+		final MenuBar.MenuItem edit = menubar.addItem("Edit", null);
+		edit.addItem("Undo", menuCommand);
+		edit.addItem("Redo", menuCommand).setEnabled(false);
+		edit.addSeparator();
+
+		edit.addItem("Cut", menuCommand);
+		edit.addItem("Copy", menuCommand);
+		edit.addItem("Paste", menuCommand);
+		edit.addSeparator();
+
+		final MenuBar.MenuItem find = edit.addItem("Find/Replace", menuCommand);
+
+		// Actions can be added inline as well, of course
+		find.addItem("Google Search", new Command() {
+			private static final long serialVersionUID = -7723967139972250713L;
+			public void menuSelected(MenuItem selectedItem) {
+				
+			}
+		});
+		find.addSeparator();
+		find.addItem("Find/Replace...", menuCommand);
+		find.addItem("Find Next", menuCommand);
+		find.addItem("Find Previous", menuCommand);
+
+		final MenuBar.MenuItem view = menubar.addItem("View", null);
+		view.addItem("Show/Hide Status Bar", menuCommand);
+		view.addItem("Customize Toolbar...", menuCommand);
+		view.addSeparator();
+
+		view.addItem("Actual Size", menuCommand);
+		view.addItem("Zoom In", menuCommand);
+		view.addItem("Zoom Out", menuCommand);
+
+		verticalLayout.addComponent(menubar);
+		
+		return verticalLayout;
 	}
+
+	private Command menuCommand = new Command() {
+		private static final long serialVersionUID = 492449983753064442L;
+
+		public void menuSelected(MenuItem selectedItem) {
+			
+		}
+	};
 }
